459bab9ee936a041924f34b16cb33d8381ccf48b
Jaqueline <jaquelinesv90@gmail.com>
2020-07-08 17:26:48 -0300

    new implementation

--- src/main/java/lesson02/JPQLQuery.java
+++ src/main/java/lesson02/JPQLQuery.java
@@ -5,6 +5,7 @@ import java.util.List;
 import javax.persistence.EntityManager;
 import javax.persistence.EntityManagerFactory;
 import javax.persistence.Persistence;
+import javax.persistence.Query;
 import javax.persistence.TypedQuery;
 
 import lesson02.model.User;
@@ -16,23 +17,47 @@ public class JPQLQuery {
 				.createEntityManagerFactory("Usuarios-PU");
 		EntityManager entityManager = entityManagerFactory.createEntityManager();
 		
-		firstSelect(entityManager);
+		//firstSelect(entityManager);
+		choosingReturn(entityManager);
+		
 		
 		
 		entityManager.close();
 		entityManagerFactory.close();
 	}
-
-	public static void firstSelect(EntityManager entityManager) {
+	
+	public static void choosingReturn(EntityManager entityManager) {
 		String jpql = "select u from User u";
-		
 		TypedQuery<User> typedQuery = entityManager.createQuery(jpql, User.class);
 		List<User> list = typedQuery.getResultList();
 		
+		for(User u : list) {
+			System.out.println(u.getId()+ "," + u.getName());
+		}
+	}
+	
+
+	public static void firstSelect(EntityManager entityManager) {
+		
+		String jpql = "select u from User u";
+		TypedQuery<User> typedQuery = entityManager.createQuery(jpql, User.class);
+		List<User> list = typedQuery.getResultList();
 		//list.forEach(u -> System.out.println(u.getId() + ","+u.getName()));
 		for(User u : list) {
-			System.out.println(u.getId()+","+u.getName());
+			System.out.println(u.getId()+ "," + u.getName());
 		}
+		
+		String jpqlSingle = "select u from User u where u.id = 1";
+		TypedQuery<User> typedQuerySingle = entityManager.createQuery(jpqlSingle, User.class);
+		User user = typedQuerySingle.getSingleResult();
+		System.out.println(user.getId()+ "," + user.getName());
+		
+		
+		String jpqlCast = "select u from User u where u.id = 1";
+		Query query = entityManager.createQuery(jpqlCast);
+		User user2 = (User)query.getSingleResult();
+		System.out.println(user2.getId()+ "," + user2.getName());
+		
 		
 	}
 	

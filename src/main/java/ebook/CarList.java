83fa6ba2c697cc0e0fc5beaf0abab866a7538cbc
Jaqueline <jaquelinesv90@gmail.com>
2020-08-09 15:24:00 -0300

    new class

---
+++ src/main/java/ebook/CarList.java
@@ -0,0 +1,32 @@
+package ebook;
+
+import java.util.List;
+
+import javax.persistence.EntityManager;
+import javax.persistence.Query;
+
+// Consulta utilizando JPQL
+// Jpql é uma extensãp da Sql, porém com características
+// da orientação a objetos,com essa linguagem não referenciamos
+// tabelas do banco de dados,apenas entidades de nosso modelo.
+public class CarList {
+	
+	public static void main(String[] args) {
+		EntityManager manager = JpaUtil.getEntityManager();
+		
+		Query query = manager
+				.createQuery(" select v from Vehicle v");
+		List<Vehicle> vehicles = query.getResultList();
+		
+		for(Vehicle v : vehicles) {
+			System.out.println(v.getCode() + " - "
+					+ v.getManufacturer() + " "
+					+ v.getYearManufacture() + " "
+					+ "R$" + v.getValue());
+		}
+		
+		manager.close();
+		JpaUtil.close();
+		
+	}
+}

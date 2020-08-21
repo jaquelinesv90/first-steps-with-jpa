db695f12738372530df2e3c6fa221224348892bf
Jaqueline <jaquelinesv90@gmail.com>
2020-08-21 15:29:36 -0300

    added comments

--- src/main/java/ebook/CarDetached.java
+++ src/main/java/ebook/CarDetached.java
@@ -7,7 +7,7 @@ import javax.persistence.EntityTransaction;
 
 /*
 	Objetos desanexados
-São objetos em um estado que não é gerenciado pelo EntityManager,mas ainda representam entidades no banco.
+São objetos em um estado que não é gerenciado pelo EntityManager, mas ainda representam entidades no banco.
 As alterações em objetos desanexados não são sincronizadas com o banco de dados.Utilizamos objetos desanexados, por exemplo,
 quando eles são expostos para alteração através de páginas web e apenas em um segundo momento o usuário solicita a 
 gravação das alterações do objeto.
@@ -30,9 +30,34 @@ public class CarDetached {
 		
 		JpaUtil.close();
 		
-	}
+		//Podemos reanexar objetos em qualquer EntityManager usando o método merge.
+		EntityManager manager1 = JpaUtil.getEntityManager();
+		EntityTransaction tx1 = manager.getTransaction();
 		
-	
-	
-	
+		tx.begin();
+		
+		Vehicle vehicle1 = manager.find(Vehicle.class, 1L);
+		tx1.commit();
+		manager.close();
+		
+		vehicle1.setValue(new BigDecimal(112_000));
+		
+		manager = JpaUtil.getEntityManager();
+		tx1 = manager1.getTransaction();
+		tx1.begin();
+		
+		//reanexamos o objeto ao novo EntityManager
+		vehicle1 = manager.merge(vehicle1);
+		
+		tx1.commit();
+		manager1.close();
+		JpaUtil.close();
+		
+		// O conteúdo do objeto desanexado é copiado para um objeto gerenciado com a mesma identidade
+		// Se o entityManager ainda não estiver gerenciando um objeto com a mesma identidade, será
+		// realizada uma consulta para encontrá-lo, ou ainda, será persistida uma nova entidade.
+		
+		// O retorno do método merge é uma instância de um objeto gerenciado.O objeto desanexo não
+		// muda de estado, ou seja, continua detached.
+	}
 }

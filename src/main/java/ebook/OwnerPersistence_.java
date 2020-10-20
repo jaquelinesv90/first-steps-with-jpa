48510f45d68db4f127e66daec9e3997ad26fc27c
Jaqueline <jaquelinesv90@gmail.com>
2020-10-19 21:31:09 -0300

    added list of vehicles in owner class

--- src/main/java/ebook/OwnerPersistence_.java
+++ src/main/java/ebook/OwnerPersistence_.java
@@ -1,5 +1,8 @@
 package ebook;
 
+import java.math.BigDecimal;
+import java.util.Date;
+
 import javax.persistence.EntityManager;
 import javax.persistence.EntityTransaction;
 
@@ -17,9 +20,31 @@ public class OwnerPersistence_ {
 		
 		manager.persist(owner);
 		
+		Vehicle vehicle1 = new Vehicle();
+		vehicle1.setManufacturer("GM");
+		vehicle1.setModel("Celta");
+		vehicle1.setYearManufacture(2015);
+		vehicle1.setModelYear(2015);
+		vehicle1.setValue(new BigDecimal(11_000));
+		vehicle1.setDateRegister(new Date());
 		
-
+		
+		manager.persist(vehicle1);
+		
+		
+		Vehicle vehicle2 = new Vehicle();
+		vehicle1.setManufacturer("VW");
+		vehicle1.setModel("Gol");
+		vehicle1.setYearManufacture(2018);
+		vehicle1.setModelYear(2018);
+		vehicle1.setValue(new BigDecimal(17_000));
+		vehicle1.setDateRegister(new Date());
+		
+		
+		manager.persist(vehicle2);
+		tx.commit();
+		manager.close();
+		JpaUtil.close();
 	}
-	
 	
 }

cb70ef1ebb2d6481f4968aba7929058dc81b93b1
Jaqueline <jaquelinesv90@gmail.com>
2020-11-17 20:29:14 -0300

    new package

---
+++ src/main/java/ebook/inheritance/Person.java
@@ -0,0 +1,49 @@
+package ebook.inheritance;
+
+import javax.persistence.Column;
+import javax.persistence.DiscriminatorColumn;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Inheritance;
+import javax.persistence.InheritanceType;
+import javax.persistence.Table;
+
+@Entity
+@Table(name = "person")
+@Inheritance(strategy = InheritanceType.SINGLE_TABLE)
+@DiscriminatorColumn(name = "type")
+public abstract class Person {
+	
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private Long code;
+	
+	@Column(length = 100, nullable = false)
+	private String name;
+
+	
+	public Person() {}
+	
+	public Person(Long code,String name) {
+		this.code = code;
+		this.name = name;
+	}
+	
+	public Long getCode() {
+		return code;
+	}
+
+	public void setCode(Long code) {
+		this.code = code;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+}

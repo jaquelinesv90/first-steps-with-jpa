0cfd825433bd6de98788c88311e64c437c5dc4b2
Jaqueline <jaquelinesv90@gmail.com>
2020-11-24 01:16:40 -0300

    new classes

---
+++ src/main/java/ebook/ClienteEmployeePersistence.java
@@ -0,0 +1,47 @@
+package ebook;
+
+import java.math.BigDecimal;
+
+import javax.persistence.EntityManager;
+import javax.persistence.EntityTransaction;
+
+import ebook.inheritance.Cliente;
+import ebook.inheritance.Employee;
+
+public class ClienteEmployeePersistence {
+	
+	public static void main(String[] args) {
+		// Obtém um EntityManager, que é responsável por
+		// gerenciar o ciclo de vida das entidades
+ 		EntityManager manager = JpaUtil.getEntityManager();
+ 		//Iniciando uma nova transação
+		EntityTransaction tx = manager.getTransaction();
+		tx.begin();
+		
+		Employee employee = new Employee();
+		employee.setName("Fernando");
+		employee.setRole("manager");
+		employee.setSalary(new BigDecimal(2_000));
+		
+
+		Cliente cliente = new Cliente();
+		cliente.setName("Mariana");
+		cliente.setMonthlyIncome(new BigDecimal(8_500));
+		cliente.setCreditLimit(new BigDecimal(2_000));
+		
+		cliente.setBlocked(true);
+		
+		
+		//executamos o método persist, passando a instancia
+		// do veículo como parametro, isso fará com que o
+		// JPA insira o objeto no banco de dados.
+		manager.persist(employee);
+		manager.persist(cliente);
+		
+		tx.commit();
+		//fechando o EntityManager e o EntityManagerFactory
+		manager.close();
+		JpaUtil.close();
+		
+	}
+}
---
+++ src/main/java/ebook/ClienteEmployeeSearching.java
@@ -0,0 +1,31 @@
+package ebook;
+
+import java.util.List;
+
+import javax.persistence.EntityManager;
+
+import ebook.inheritance.Cliente;
+import ebook.inheritance.Person;
+
+public class ClienteEmployeeSearching {
+	
+	public static void main(String[] args) {
+		EntityManager manager = JpaUtil.getEntityManager();
+		
+		List<Person> people = manager.createQuery("SELECT P FROM PERSON P",Person.class).getResultList();
+		
+		for(Person person : people) {
+			System.out.println(person.getName());
+			
+			if(person instanceof Cliente) {
+				System.out.println("it's a cliente");
+			}else {
+				System.out.println("it's employee");
+			}
+		}
+		
+		manager.close();
+		JpaUtil.close();
+	}
+
+}

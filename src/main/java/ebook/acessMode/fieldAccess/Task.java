b207793e75492cb05d8390eac472424f57018a25
Jaqueline <jaquelinesv90@gmail.com>
2020-12-01 18:56:00 -0300

    new packages created

---
+++ src/main/java/ebook/acessMode/fieldAccess/Task.java
@@ -0,0 +1,47 @@
+package ebook.acessMode.fieldAccess;
+
+import java.time.LocalDateTime;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+@Entity
+@Table(name = "task")
+public class Task {
+	
+	@Id
+	@GeneratedValue
+	private Long code;
+	
+	private String description;
+	
+	private LocalDateTime deadline;
+
+	
+	
+	public Long getCode() {
+		return code;
+	}
+
+	public void setCode(Long code) {
+		this.code = code;
+	}
+
+	public String getDescription() {
+		return description;
+	}
+
+	public void setDescription(String description) {
+		this.description = description;
+	}
+
+	public LocalDateTime getDeadline() {
+		return deadline;
+	}
+
+	public void setDeadline(LocalDateTime deadline) {
+		this.deadline = deadline;
+	}
+}

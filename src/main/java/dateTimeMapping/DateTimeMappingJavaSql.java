21ffc4fea0696c8144942844cd18edc52498ec66
Jaqueline <jaquelinesv90@gmail.com>
2021-01-07 19:57:45 -0300

    new example of Date and time mapping

---
+++ src/main/java/dateTimeMapping/DateTimeMappingJavaSql.java
@@ -0,0 +1,54 @@
+package dateTimeMapping;
+
+import java.sql.Time;
+import java.sql.Timestamp;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+/* java.sql.Date é mapeado para SQL DATE
+ * java.sql.TIME é mapeado para SQL TIME
+ * java.sql.TIMESTAMP é mapeado para SQL TIMESTAMP
+ */
+
+@Entity
+public class DateTimeMappingJavaSql {
+	
+	@Id
+	@GeneratedValue(strategy = GenerationType.AUTO)
+	private Long id;
+	
+	private java.sql.Date sqlDate;
+	
+	private Time sqlTime;
+	
+	private Timestamp sqlTimestamp;
+	
+	
+
+	public java.sql.Date getSqlDate() {
+		return sqlDate;
+	}
+
+	public void setSqlDate(java.sql.Date sqlDate) {
+		this.sqlDate = sqlDate;
+	}
+
+	public Time getSqlTime() {
+		return sqlTime;
+	}
+
+	public void setSqlTime(Time sqlTime) {
+		this.sqlTime = sqlTime;
+	}
+
+	public Timestamp getSqlTimestamp() {
+		return sqlTimestamp;
+	}
+
+	public void setSqlTimestamp(Timestamp sqlTimestamp) {
+		this.sqlTimestamp = sqlTimestamp;
+	}
+}
--- src/main/java/ebook/callbackAndEntityListeners/PostPersist.java
+++ src/main/java/ebook/callbackAndEntityListeners/PostPersist.java
@@ -1,9 +1,35 @@
 package ebook.callbackAndEntityListeners;
 
+import java.time.LocalDate;
+import java.time.LocalDateTime;
+
+import javax.persistence.EntityManager;
+import javax.persistence.EntityTransaction;
+
+import ebook.JpaUtil;
+
 public class PostPersist {
 	
 	public static void main(String[] args) {
 		
+		EntityManager manager = JpaUtil.getEntityManager();
+		EntityTransaction tx = manager.getTransaction();
+		tx.begin();
+		
+		Animal animal = new Animal();
+		animal.setName("Dog");
+		animal.setDateBirthday(LocalDate.now().minusYears(5));
+		animal.setDateLastUpdate(LocalDateTime.now());
+		
+		System.out.println("age before persistence: " + animal.getAge());
+		
+		System.out.println(animal);
+
+		System.out.println("age after persistence: " + animal.getAge());
+		
+		tx.commit();
+		manager.close();
+		JpaUtil.close();
 	}
 
 }

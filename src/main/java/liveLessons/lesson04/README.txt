41851d7c56df9b09c0d966193fffc27d7a177aa7
Jaqueline <jaquelinesv90@gmail.com>
2021-06-22 16:07:40 -0300

    new package class

---
+++ src/main/java/liveLessons/lesson04/README.txt
@@ -0,0 +1,4 @@
+CRITERIA API
+
+É uma subAPI do JPA dentro do JPA, que vamos utilizar para fazer consultas de forma programática,
+através de métodos.
---
+++ src/main/java/liveLessons/lesson04/model/Configuration.java
@@ -0,0 +1,57 @@
+package liveLessons.lesson04.model;
+
+import javax.persistence.Id;
+import javax.persistence.MapsId;
+import javax.persistence.OneToOne;
+
+//@Entity
+public class Configuration {
+	
+		@Id
+		private Integer id;
+		
+		// anotação mapsId: quer dizer que a minha chave estrangeira é também 
+		//minha chave primária
+		@MapsId
+		@OneToOne
+		private User user;
+		
+		private boolean getNotification;
+		
+		private boolean logOutAutomatically;
+		
+		
+		public Integer getId() {
+			return id;
+		}
+
+		public void setId(Integer id) {
+			this.id = id;
+		}
+
+		public User getUser() {
+			return user;
+		}
+
+		public void setUser(User user) {
+			this.user = user;
+		}
+
+		public boolean isGetNotification() {
+			return getNotification;
+		}
+
+		public void setGetNotification(boolean getNotification) {
+			this.getNotification = getNotification;
+		}
+
+		public boolean isLogOutAutomatically() {
+			return logOutAutomatically;
+		}
+
+		public void setLogOutAutomatically(boolean logOutAutomatically) {
+			this.logOutAutomatically = logOutAutomatically;
+		}
+		
+		
+}
